2834f7dea27c95421740b1fd5052c34dd4b84d19
zcammack <zack.cammack@gmail.com>
2020-11-19 17:50:49 -0600

    Setup Devise. Working on facebook authentication now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  
+authenticated :user do
+    root 'home#index', as: 'authenticated_root'
+  end
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
